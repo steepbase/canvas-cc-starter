617b3bf9970fc5165aff8dae12951c3600cea71b
Balint Kleri <b@balintbrews.com>
2025-09-27 17:34:19 +0200

    feat(vite.config): set up @/lib/utils as an alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
+const DIR = path.dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@/lib/utils": path.resolve(DIR, "./src/lib/utils.ts"),
+    },
+  },
 });
